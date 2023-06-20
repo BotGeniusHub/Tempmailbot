d3fc6df897b0b656d6346c78aac1230d01fc3c65
BotGeniusHub <136921156+BotGeniusHub@users.noreply.github.com>
2023-06-20 22:30:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-API_ID = 16743442  # Replace with your API ID
-API_HASH = "12bbd720f4097ba7713c5e40a11dfd2a"  # Replace with your API hash
-BOT_TOKEN = "5992274138:AAGNoQ5XDjO_1j3V7xplNAusU0iSYojaNec"  # Replace with your bot token
+API_ID = 123456  # Replace with your API ID
+API_HASH = ""  # Replace with your API hash
+BOT_TOKEN = ""  # Replace with your bot token
