57bab3555fe8bd5ead390b08fa07e2cb924a4df0
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-20 19:21:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-TELEGRAM_TOKEN = "5992274138:AAGNoQ5XDjO_1j3V7xplNAusU0iSYojaNec"
+# config.py
+
+API_ID = 16743442  # Replace with your API ID
+API_HASH = "12bbd720f4097ba7713c5e40a11dfd2a"  # Replace with your API hash
+BOT_TOKEN = "5992274138:AAGNoQ5XDjO_1j3V7xplNAusU0iSYojaNec"  # Replace with your bot token
