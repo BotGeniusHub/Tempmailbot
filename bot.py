87fe641af1ef04a6ee81930e9e212f60e69935c3
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-20 19:20:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,34 @@
 import os
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackQueryHandler, Updater
-from config import TELEGRAM_TOKEN
+from pyrogram import Client, filters, idle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import API_ID, API_HASH, BOT_TOKEN
 
-def main():
-    updater = Updater(token="5992274138:AAGNoQ5XDjO_1j3V7xplNAusU0iSYojaNec", use_context=True)
+app = Client("tempmailbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN) 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the TempMailBot!")
+@app.on_message(filters.command("start"))
+def start(client, message):
+    client.send_message(chat_id=message.chat.id, text="Welcome to the TempMailBot!")
 
 
-def help(update, context):
+@app.on_message(filters.command("help"))
+def help(client, message):
     help_text = "This bot provides temporary email addresses. Use /generate to get a new address."
-    context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
+    client.send_message(chat_id=message.chat.id, text=help_text)
 
 
-def generate(update, context):
+@app.on_message(filters.command("generate"))
+def generate(client, message):
     api_url = "https://www.1secmail.com/api/v1/"
     response = requests.get(api_url + "/?action=genRandomMailbox&count=1")
     if response.status_code == 200:
         email_address = response.json()[0]
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Your temporary email address is: {email_address}")
+        client.send_message(chat_id=message.chat.id, text=f"Your temporary email address is: {email_address}")
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Failed to generate a temporary email address.")
+        client.send_message(chat_id=message.chat.id, text="Failed to generate a temporary email address.")
 
 
-def inbox(update, context):
+@app.on_message(filters.command("inbox"))
+def inbox(client, message):
     api_url = "https://www.1secmail.com/api/v1/"
     email_address = "example@example.com"  # Replace with the actual email address
     response = requests.get(api_url + f"/?action=getMessages&login={email_address}&domain=1secmail.com")
@@ -34,68 +37,49 @@ def inbox(update, context):
         if messages:
             for message in messages:
                 subject = message["subject"]
-                context.bot.send_message(chat_id=update.effective_chat.id, text=f"Subject: {subject}")
+                client.send_message(chat_id=message.chat.id, text=f"Subject: {subject}")
         else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="No messages in the inbox.")
+            client.send_message(chat_id=message.chat.id, text="No messages in the inbox.")
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Failed to retrieve inbox messages.")
+        client.send_message(chat_id=message.chat.id, text="Failed to retrieve inbox messages.")
 
 
-def delete_mail(update, context):
+@app.on_message(filters.command("delete"))
+def delete_mail(client, message):
     api_url = "https://www.1secmail.com/api/v1/"
     email_address = "example@example.com"  # Replace with the actual email address
     email_id = "12345"  # Replace with the actual email ID of the email you want to delete
     response = requests.get(api_url + f"/?action=deleteMessage&login={email_address}&domain=1secmail.com&id={email_id}")
     if response.status_code == 200:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Email deleted successfully.")
+        client.send_message(chat_id=message.chat.id, text="Email deleted successfully.")
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Failed to delete email.")
+        client.send_message(chat_id=message.chat.id, text="Failed to delete email.")
 
 
-def inline_buttons(update, context):
+@app.on_message(filters.command("inline"))
+def inline_buttons(client, message):
     keyboard = [
         [InlineKeyboardButton("Generate Temporary Email", callback_data='generate')],
         [InlineKeyboardButton("Inbox", callback_data='inbox')],
         [InlineKeyboardButton("Help", callback_data='help')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Please choose an option:', reply_markup=reply_markup)
+    client.send_message(chat_id=message.chat.id, text='Please choose an option:', reply_markup=reply_markup)
 
 
-def button_callback(update, context):
-    query = update.callback_query
+@app.on_callback_query()
+def button_callback(client, callback_query):
+    query = callback_query
     if query.data == 'generate':
-        generate(update, context)
+        generate(client, query.message)
     elif query.data == 'inbox':
-        inbox(update, context)
+        inbox(client, query.message)
     elif query.data == 'help':
-        help(update, context)
+        help_text = "This bot provides temporary email addresses. Use /generate to get a new address."
+        client.send_message(chat_id=query.message.chat.id, text=help_text)
     query.answer()
 
 
-def main():
-    updater = Updater(token=os.environ.get("5992274138:AAGNoQ5XDjO_1j3V7xplNAusU0iSYojaNec"), use_context=True)
-    dispatcher = updater.dispatcher
-
-    start_handler = CommandHandler('start', start)
-    help_handler = CommandHandler('help', help)
-    generate_handler = CommandHandler('generate', generate)
-    inbox_handler = CommandHandler('inbox', inbox)
-    delete_mail_handler = CommandHandler('delete', delete_mail)
-    inline_buttons_handler = CallbackQueryHandler(inline_buttons)
-    button_callback_handler = CallbackQueryHandler(button_callback)
-
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(generate_handler)
-    dispatcher.add_handler(inbox_handler)
-    dispatcher.add_handler(delete_mail_handler)
-    dispatcher.add_handler(inline_buttons_handler)
-    dispatcher.add_handler(button_callback_handler)
-
-    updater.start_polling()
-    updater.idle()
-
-
 if __name__ == '__main__':
-    main()
+    app.run()
+idle() 
